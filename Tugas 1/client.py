53103bcd7e1fda9811967930eef94e7917b6ebf1
Rana Wijdan Naim <ranawijdannaim@gmail.com>
2020-03-03 01:25:05 +0700

    update lagi euy 2

--- Tugas 1/client.py
+++ Tugas 1/client.py
@@ -5,7 +5,7 @@ import socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Connect the socket to the port where the server is listening
-server_address = ('127.0.0.1', 31001)
+server_address = ('localhost', 31002)
 print(f"connecting to {server_address}")
 sock.connect(server_address)
 
@@ -19,9 +19,9 @@ try:
     amount_received = 0
     amount_expected = len(message)
     while amount_received < amount_expected:
-        data = sock.recv(16)
+        data = sock.recv(64)
         amount_received += len(data)
-        print(f"{data}")
+        print(f"{data.decode()}")
 finally:
     print("closing")
     sock.close()
--- Tugas 1/server2.py
+++ Tugas 1/server2.py
@@ -3,7 +3,7 @@ import socket
 # Create a TCP/IP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # Bind the socket to the port
-server_address = ('127.0.0.1', 31000)
+server_address = ('localhost', 31001)
 print(f"starting up on {server_address}")
 sock.bind(server_address)
 # Listen for incoming connections
@@ -16,7 +16,7 @@ while True:
     # Receive the data in small chunks and retransmit it
     while True:
         data = connection.recv(64)
-        print(f"received {data.decode()}")
+        print(f"received {data}")
         if data:
             print("sending back data")
             connection.sendall(data)
