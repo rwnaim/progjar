81a545a126ac3786f507013954678f1e5dc9bc22
rwnaim <rwnaim@WIJDANNAIM-LAPTOP.localdomain>
2020-05-16 04:47:08 +0700

    update lagiii

--- Tugas 9/http.py
+++ Tugas 9/http.py
@@ -14,7 +14,7 @@ class HttpServer:
         self.types['.txt'] = 'text/plain'
         self.types['.html'] = 'text/html'
 
-    def response(self, kode=404, message='Not Found', messagebody=bytes(), headers={}):
+    def response(self, kode=404, message='Not Found', messagebody='', headers={}):
         tanggal = datetime.now().strftime('%c')
         resp = []
         resp.append("HTTP/1.0 {} {}\r\n".format(kode, message))
@@ -25,51 +25,36 @@ class HttpServer:
         for kk in headers:
             resp.append("{}:{}\r\n".format(kk, headers[kk]))
         resp.append("\r\n")
-
-        response_headers = ''
+        resp.append("{}".format(messagebody))
+        response_str = ''
         for i in resp:
-            response_headers = "{}{}".format(response_headers, i)
-        # menggabungkan resp menjadi satu string dan menggabungkan dengan messagebody yang berupa bytes
-        # response harus berupa bytes
-        # message body harus diubah dulu menjadi bytes
-        if (type(messagebody) is not bytes):
-            messagebody = messagebody.encode()
-
-        response = response_headers.encode() + messagebody
-        # response adalah bytes
-        return response
+            response_str = "{}{}".format(response_str, i)
+        return response_str
 
     def proses(self, data):
 
         requests = data.split("\r\n")
-        # print(requests)
-
         baris = requests[0]
-        # print(baris)
 
-        all_headers = [n for n in requests[1:] if n != '']
-
+        print
+        baris
         j = baris.split(" ")
         try:
             method = j[0].upper().strip()
             if (method == 'GET'):
                 object_address = j[1].strip()
-                return self.http_get(object_address, all_headers)
-            if (method == 'POST'):
-                object_address = j[1].strip()
-                return self.http_post(object_address, all_headers)
+                return self.http_get(object_address)
             else:
                 return self.response(400, 'Bad Request', '', {})
         except IndexError:
             return self.response(400, 'Bad Request', '', {})
 
-    def http_get(self, object_address, headers):
+    def http_get(self, object_address):
         files = glob('./*')
         thedir = '.'
         if thedir + object_address not in files:
             return self.response(404, 'Not Found', '', {})
-        fp = open(thedir + object_address, 'rb')  # rb => artinya adalah read dalam bentuk binary
-        # harus membaca dalam bentuk byte dan BINARY
+        fp = open(thedir + object_address, 'r')
         isi = fp.read()
 
         fext = os.path.splitext(thedir + object_address)[1]
@@ -80,11 +65,6 @@ class HttpServer:
 
         return self.response(200, 'OK', isi, headers)
 
-    def http_post(self, object_address, headers):
-        headers = {}
-        isi = "kosong"
-        return self.response(200, 'OK', isi, headers)
-
 
 # >>> import os.path
 # >>> ext = os.path.splitext('/ak/52.png')
@@ -92,8 +72,26 @@ class HttpServer:
 if __name__ == "__main__":
     httpserver = HttpServer()
     d = httpserver.proses('GET testing.txt HTTP/1.0')
-    print(d)
+    print
+    d
     d = httpserver.http_get('testing2.txt')
-    print(d)
+    print
+    d
     d = httpserver.http_get('testing.txt')
-    print(d)
+    print
+    d
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
