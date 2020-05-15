c1727b682f884eac6d0a9b59a46587365558918c
rwnaim <rwnaim@WIJDANNAIM-LAPTOP.localdomain>
2020-05-16 04:51:12 +0700

    update lagi

---
+++ Tugas 10/runserver_async.sh
@@ -0,0 +1,10 @@
+#jalankan 5 async_server
+
+python3 server_thread_http.py 9002 &
+python3 server_thread_http.py 9003 &
+python3 server_thread_http.py 9004 &
+python3 server_thread_http.py 9005 &
+
+
+
+
