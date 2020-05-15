b4e04d8d7eab3912294715d781356a4f2454ba40
rwnaim <rwnaim@WIJDANNAIM-LAPTOP.localdomain>
2020-05-16 04:46:04 +0700

    update lagii

--- Tugas 10/lb.py
+++ Tugas 10/lb.py
@@ -2,18 +2,17 @@ import socket
 import time
 import sys
 import asyncore
+import logging
 
 
 class BackendList:
 	def __init__(self):
 		self.servers=[]
-		# self.servers.append(('127.0.0.1',9001))
 		self.servers.append(('127.0.0.1',9002))
 		self.servers.append(('127.0.0.1',9003))
 		self.servers.append(('127.0.0.1',9004))
 		self.servers.append(('127.0.0.1',9005))
 		self.current=0
-
 	def getserver(self):
 		s = self.servers[self.current]
 		self.current=self.current+1
@@ -21,30 +20,33 @@ class BackendList:
 			self.current=0
 		return s
 
+
 class Backend(asyncore.dispatcher_with_send):
 	def __init__(self,targetaddress):
-	    asyncore.dispatcher_with_send.__init__(self)
-	    self.create_socket(socket.AF_INET,socket.SOCK_STREAM)
-	    self.connect(targetaddress)
-	    self.connection = self
+		asyncore.dispatcher_with_send.__init__(self)
+		self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.connect(targetaddress)
+		self.connection = self
+
 	def handle_read(self):
-	    try:
-	    	self.client_socket.sendall(self.recv(8192))
-	    except:
- 			pass
-	def handle_close(self):
-	    try:
-	    	self.close()
-	    	self.client_socket.close()
-	    except:
+		try:
+			self.client_socket.send(self.recv(8192))
+		except:
 			pass
+	def handle_close(self):
+		try:
+			self.close()
+			self.client_socket.close()
+		except:
+			pass
+
 
 class ProcessTheClient(asyncore.dispatcher):
 	def handle_read(self):
-	   data = self.recv(8192)
-	   if data:
-		   self.backend.client_socket = self
-		   self.backend.sendall(data)
+		data = self.recv(8192)
+		if data:
+			self.backend.client_socket = self
+			self.backend.send(data)
 	def handle_close(self):
 		self.close()
 
@@ -74,11 +76,14 @@ class Server(asyncore.dispatcher):
 			handler.backend = backend
 
 
-
 def main():
-	svr = Server()
+	portnumber=44444
+	try:
+		portnumber=int(sys.argv[1])
+	except:
+		pass
+	svr = Server(portnumber)
 	asyncore.loop()
 
 if __name__=="__main__":
 	main()
-
